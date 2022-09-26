2646efff83d1099296a5661c72ad34d378f0f60f
Utsa-v <114085787+Utsa-v@users.noreply.github.com>
2022-09-27 02:02:54 +0530

    Exercises

---
+++ Java/src/Q1.java
@@ -0,0 +1,19 @@
+public class Q1 {
+	
+	static int age = 23;
+	static {
+		System.out.println("Utsav");
+	}
+
+	public static void get_lastname() {
+		System.out.println("Sharma");
+
+	}
+
+	public static void main(String[] args) {
+		get_lastname();
+		System.out.println(Q1.age);
+
+	}
+
+}
